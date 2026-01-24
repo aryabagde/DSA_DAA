966a2b28665aebb67177207fcb4ffadf7644c908
aryabagde <akruyhau@gmail.com>
2026-01-25 04:18:10 +0530

    starting with simple snippets

---
+++ SimpleSnippets/variables.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    //types of variables inn c++
+    int a = 10;               // Integer type
+    float b = 5.5;           // Floating point type
+    double c = 9.99;         // Double precision floating point type
+    char d = 'A';           // Character type
+    bool e = true;          // Boolean type
+    string f = "Hello";     // String type
+
+    return 0;
+}
