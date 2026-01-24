f76127abaa637a332846b2f5ba80da6043b10e2a
aryabagde <akruyhau@gmail.com>
2026-01-24 17:47:53 +0530

    8th error caught and logic changed

--- Codeforces/8.PetyaandStrings.cpp
+++ Codeforces/8.PetyaandStrings.cpp
@@ -4,26 +4,32 @@ using namespace std;
 int main(){
 
     string inp1, inp2;
-    int count1, count2;
-    count1 =0;
-    count2 =0;
+    int flag;
+    flag = 0;
     cin>>inp1>>inp2;
     for(int i=0; i<inp1.length(); i++){
-        count1 += tolower(inp1[i]);
+        inp1[i] = (char) tolower(inp1[i]);
     }
     for(int j=0; j<inp2.length(); j++){
-        count2 += tolower(inp2[j]);
+        inp2[j] = (char) tolower(inp2[j]);
     }
-    if(count1<count2 ){
-        cout<<"-1"<<endl;
+    for(int k=0; k<inp1.length(); k++){
+        if(inp1[k] == inp2[k]){
+            continue;
+        }
+        else{
+            if(inp1[k]<inp2[k]){
+                flag = -1;
+                break;
+            }
+            else{
+                flag = 1;
+                break;
+            }
+        }
     }
-    else if(count1>count2){
-        cout<<"1"<<endl;
-    }
-    else{
-        cout<<"0"<<endl;
-    }
-
-
+    cout<<flag;
+    //found 2 erros the first attempt i only calculated for sum of ascii vavlues and it went wrong since ascii value of ab and ba is same so that error
+    //second attempt compared each char by char and formed if else and flag situation to solve imp:: forgot to use break statement so even after turning flag 1 it continued in the loop and turned into -1
     return 0;
 }
