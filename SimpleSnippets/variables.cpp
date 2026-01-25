fffdf805ac2c9257e4c0b72dd95c7b2e2d9d0b40
aryabagde <akruyhau@gmail.com>
2026-01-25 16:14:38 +0530

    all types of variables

--- SimpleSnippets/variables.cpp
+++ SimpleSnippets/variables.cpp
@@ -1,15 +1,74 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+/* ---------- Global Variable ---------- */
+int globalVar = 100;
 
-    //types of variables inn c++
-    int a = 10;               // Integer type
-    float b = 5.5;           // Floating point type
-    double c = 9.99;         // Double precision floating point type
-    char d = 'A';           // Character type
-    bool e = true;          // Boolean type
-    string f = "Hello";     // String type
+/* ---------- User-defined type ---------- */
+struct Student {
+    int roll;
+};
+
+void staticExample() {
+    static int staticVar = 0;   // static variable
+    staticVar++;
+    cout << "Static variable value: " << staticVar << endl;
+}
+
+int main() {
+
+    /* ---------- Primitive / Built-in Variables ---------- */
+    int a = 10;
+    float b = 3.5f;
+    double c = 3.14159;
+    char d = 'A';
+    bool e = true;
+
+    /* ---------- const Variable ---------- */
+    const int CONST_VAR = 50;
+
+    /* ---------- auto Variable ---------- */
+    auto autoVar = 20;      // compiler deduces int
+
+    /* ---------- Local Variable ---------- */
+    int localVar = 5;
+
+    /* ---------- Derived Variables ---------- */
+    int arr[3] = {1, 2, 3};     // array
+    int* ptr = &a;             // pointer
+    int& ref = a;              // reference
+
+    /* ---------- User-defined Variable ---------- */
+    Student s1;
+    s1.roll = 101;
+
+    /* ---------- Output ---------- */
+    cout << "Primitive variables:" << endl;
+    cout << a << " " << b << " " << c << " " << d << " " << e << endl;
+
+    cout << "Const variable: " << CONST_VAR << endl;
+    cout << "Auto variable: " << autoVar << endl;
+    cout << "Local variable: " << localVar << endl;
+
+    cout << "Global variable: " << globalVar << endl;
+
+    cout << "Array elements: ";
+    for(int i = 0; i < 3; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    cout << "Pointer value (address of a): " << ptr << endl;
+    cout << "Pointer dereference (*ptr): " << *ptr << endl;
+
+    cout << "Reference variable (ref): " << ref << endl;
+
+    cout << "User-defined variable (Student roll): " << s1.roll << endl;
+
+    /* ---------- Static Variable Demo ---------- */
+    staticExample();
+    staticExample();
+    staticExample();
 
     return 0;
 }
