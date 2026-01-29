455e399cc38c6cdba395069247ef207edec4953c
aryabagde <akruyhau@gmail.com>
2026-01-29 22:13:19 +0530

    very little progress

---
+++ Codeforces/14.Stonesonthetable.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int c, front, rear;
+    front = -1;
+    rear = -1;
+    cin>>c;
+    int arr[c];
+    for(int i=0; i<c; i++){
+        cin>>arr[i];
+    }
+    while(front = c-1)
+    return 0;
+}
