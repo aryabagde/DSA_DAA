0b2edfc84810a2e78d8b21490902e5d382febb3a
aryabagde <akruyhau@gmail.com>
2026-07-03 15:52:21 +0530

    rough work

--- StriverDSAsheet/first.cpp
+++ StriverDSAsheet/first.cpp
@@ -1,7 +1,9 @@
-#include <iostream>
+#include <bits/stdc++.h> //this header is used in competitive programming competitions, it imports almost every important cpp library, it is non standard and also causes bloating
 using namespace std;
 
 int main(){
-    
+    int a;
+    cin>>a;
+    cout<<a;
     return 0;
 }
