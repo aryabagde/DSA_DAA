7c35538d84a12abf9920a85eb7acaa8eafa99691
aryabagde <akruyhau@gmail.com>
2026-07-09 11:50:19 +0530

    lessgo easy

---
+++ StriverDSAsheet/kajukatli.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    ////combining both
+    int n,k,m;
+    cin>>n;
+    k = n+(n-1);
+    m = 1;
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=k; j++){
+            if(j<=(n-m)){
+                cout<<" ";
+            }
+            else if(j>=(n+m)){
+                cout<<" ";
+            }
+            else{
+                cout<<"*";
+            }
+        }
+        cout<<endl;
+        m++;
+    }
+    m=n;
+    for(int i=n; i>=1; i--){
+        for(int j=k; j>=1; j--){
+            if(j<=n-m){
+                cout<<" ";
+            }
+            else if(j>=n+m){
+                cout<<" ";
+            }
+            else{
+                cout<<"*";
+            }
+        }
+        cout<<endl;
+        m--;
+    }
+
+    return 0;
+}
