35dd84003aa3b964a6e3ec329eb1601c3cc71d37
aryabagde <akruyhau@gmail.com>
2026-07-09 12:49:20 +0530

    done

--- StriverDSAsheet/righthalfrombus.cpp
+++ StriverDSAsheet/righthalfrombus.cpp
@@ -1,9 +1,34 @@
-#inclulde <bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main(){
-    int n;
+    int n,m;
     cin>>n;
-    
+    m =1;
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n; j++){
+            if(j>m){
+                cout<<" ";
+            }
+            else{
+                cout<<"*";
+            }
+        }
+        cout<<endl;
+        m++;
+    }
+    m=n;
+    for(int i=n; i>=1; i--){
+        for(int j=1; j<=n; j++){
+            if(j<m){
+                cout<<"*";
+            }
+            else{
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+        m--;
+    }
     return 0;
 }
