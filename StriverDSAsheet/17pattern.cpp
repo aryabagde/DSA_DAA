821bacadd007fbe718e576dcc08b29725fa2ed16
aryabagde <akruyhau@gmail.com>
2026-07-14 13:04:05 +0530

    done 17 pattern

--- StriverDSAsheet/17pattern.cpp
+++ StriverDSAsheet/17pattern.cpp
@@ -3,26 +3,28 @@ using namespace std;
 
 int main(){
 
-    int n, k;
+    int n, k, m;
     cin>>n;
-    k = 2*n -1;
+    k = 2*n-1;
+    m = 1;
     char count;
-    int m = 1;
     for(int i=1; i<=n; i++){
         count = 'A';
         for(int j=1; j<=k; j++){
-            if(m<n){
+            if(j<=(n-m)){
                 cout<<" ";
-
             }
-            else if(j>k){
+            else if(j>=(n+m)){
                 cout<<" ";
             }
             else{
                 cout<<count;
+                if(j<n) count++;
+                else count--;
             }
         }
         cout<<endl;
+        m++;
     }
 
     return 0;
