75a23042852f598134bcf580bf87b3a56303444a
aryabagde <akruyhau@gmail.com>
2026-07-14 12:29:05 +0530

    wrong

--- StriverDSAsheet/17pattern.cpp
+++ StriverDSAsheet/17pattern.cpp
@@ -2,5 +2,28 @@
 using namespace std;
 
 int main(){
+
+    int n, k;
+    cin>>n;
+    k = 2*n -1;
+    char count;
+    int m = 1;
+    for(int i=1; i<=n; i++){
+        count = 'A';
+        for(int j=1; j<=k; j++){
+            if(m<n){
+                cout<<" ";
+
+            }
+            else if(j>k){
+                cout<<" ";
+            }
+            else{
+                cout<<count;
+            }
+        }
+        cout<<endl;
+    }
+
     return 0;
 }
