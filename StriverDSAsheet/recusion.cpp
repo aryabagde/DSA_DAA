785196a1994af94d5962aae35ad0936f20ed69ba
aryabagde <akruyhau@gmail.com>
2026-07-31 17:54:35 +0530

    recursion 1 done

--- StriverDSAsheet/recusion.cpp
+++ StriverDSAsheet/recusion.cpp
@@ -16,7 +16,7 @@ void basic(){      // basic recursive function
 }
 
 string ch = "Artya";
-void name_five(int n){
+void name_five(int n){   // time complexity will be O(n)
     int static ct = 1;
     cout<<ch<<endl;
     if(ct == n) return;
@@ -24,10 +24,28 @@ void name_five(int n){
     name_five(n);
 }
 
-void name_five_without_static(int i, int n){
+void name_five_without_static(int i, int n){  //time and stack space complexity will be O(n)
     if(i>n) return;
     cout<<ch<<endl;
     name_five_without_static(i+1, n);
+}
+
+void print_numbers(int i,int n){
+    if(i>n) return;
+    cout<<i<<" ";
+    print_numbers(i+1, n);
+}
+
+void print_numbers_reverse(int n){
+    if(n <1) return;
+    cout<<n<<" ";
+    print_numbers_reverse(n-1);
+}
+
+void backtrack_print_numbers(int i, int n){      // in backtracking the last person should get executed first then we wil backtrack 
+    if(i<1) return;
+    backtrack_print_numbers(i-1, n);
+    cout<<i<<" ";
 }
 
 int main(){
@@ -35,6 +53,9 @@ int main(){
     cin>>n;
     //basic();
     //name_five(n);
-    name_five_without_static(1, n);
+    //name_five_without_static(1, n);
+    //print_numbers(1, n);
+    //print_numbers_reverse(n);
+    backtrack_print_numbers(n, n);
     return 0;
 }
